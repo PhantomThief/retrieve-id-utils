9b7835faf71c1437917e8be19a70835c79dcc396
w.vela <vela@kuaishou.com>
2016-05-10 17:40:26 +0800

    add getOnly/setOnly

--- src/main/java/com/github/phantomthief/util/IMultiDataAccess.java
+++ src/main/java/com/github/phantomthief/util/IMultiDataAccess.java
@@ -7,11 +7,33 @@ import static java.util.Collections.emptyMap;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * @author w.vela
  */
 public interface IMultiDataAccess<K, V> {
+
+    static <K, V> IMultiDataAccess<K, V> getOnly(Function<Collection<K>, Map<K, V>> getFunction) {
+        return new IMultiDataAccess<K, V>() {
+
+            @Override
+            public Map<K, V> get(Collection<K> keys) {
+                return getFunction.apply(keys);
+            }
+        };
+    }
+
+    static <K, V> IMultiDataAccess<K, V> setOnly(Consumer<Map<K, V>> setFunction) {
+        return new IMultiDataAccess<K, V>() {
+
+            @Override
+            public void set(Map<K, V> dataMap) {
+                setFunction.accept(dataMap);
+            }
+        };
+    }
 
     /**
      * @param keys without null.
